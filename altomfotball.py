f99ef57bf2b18ef34d238cf2bc2749f100536c85
Mikkel Gjestrud <bilget@mail.uio.no>
2023-01-22 20:14:22 +0100

    updated readme slightly, and used .env file to get password instead of .bash:profile

--- altomfotball.py
+++ altomfotball.py
@@ -1,12 +1,19 @@
-import pandas as pd
-from bs4 import BeautifulSoup
-import requests
-from datetime import datetime
 import os
+from dotenv import load_dotenv
+import smtplib
+import ssl
+import pandas as pd
+import requests
+import time
+import datetime as dt
+from bs4 import BeautifulSoup
+from email.message import EmailMessage
+
+load_dotenv(".env")
 
 username = os.environ.get('USERNAME')
 password = os.environ.get('PASSWORD')
-# print(username, password)
+print(username, password)
 
 response = requests.get("https://www.altomfotball.no/elementsCommonAjax.do?cmd=fixturesContent&seasonId=344&teamId=732&month=all&useFullUrl=false")
 # response = requests.get("https://www.altomfotball.no/elementsCommonAjax.do?cmd=fixturesContent&tournamentId=230&month=all&useFullUrl=false")
@@ -18,13 +25,9 @@ else:
 soup = BeautifulSoup(response.content, 'html.parser')
 
 
-# nb_links = len(soup.find_all('a'))
-# print(soup.get_text())
-# all_dates = soup.select('td:nth-child(1) > span:nth-child(1)')
-
 all_dates = soup.select('td:nth-child(1) > span')
 dates = [r.text.split(' ')[0] for r in all_dates]
-dates = [datetime.strptime(r, '%d.%m.%Y') for r in dates]
+dates = [dt.datetime.strptime(r, '%d.%m.%Y') for r in dates]
 # print(dates)
 
 all_home = soup.select('td:nth-child(4) > a ')
@@ -52,3 +55,37 @@ data['Channel'] = channels
 
 print(data)
 
+
+
+email_sender = username; email_password = password
+email_receiver = 'm.gjestrud@gmail.com'
+
+
+def send_email():
+
+	subject = 'Altomfotball'
+	body = """
+	Hei!
+	Dette er en test av å sende epost fra Python.
+	Mvh
+	Marius"""
+
+	em = EmailMessage()
+	em['From'] = email_sender
+	em['To'] = email_receiver
+	em['Subject'] = subject
+	em.set_content(body)
+
+	context = ssl.create_default_context()
+
+	with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+		smtp.login(email_sender, email_password)
+		smtp.sendmail(email_sender, email_receiver, em.as_string())
+
+send_time = dt.datetime(2023,1,22,19,52,0) # set your sending time in UTC
+# send_email()
+
+
+
+current_date = dt.datetime.date(dt.datetime.now())
+print(current_date)
